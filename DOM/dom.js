cebbad5cef3e8234ef9fbecf4dbf99b24d354f98
Arjunsingh-b <arjunsingh.b1@gmail.com>
2023-05-09 17:37:22 +0530

    Filter HTML

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,55 +1,80 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+    var form = document.getElementById('addForm');
+    var itemList = document.getElementById('items');
+    var filter = document.getElementById('filter');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
+    // Form submit event
+    form.addEventListener('submit', addItem);
+    // Delete event
+    itemList.addEventListener('click', removeItem);
 
-// Add item
-function addItem(e){
-  e.preventDefault();
+    filter.addEventListener('keyup', filterItems);
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+    // Add item
+    function addItem(e){
+    e.preventDefault();
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+    // Get input value
+    var newItem = document.getElementById('item').value;
+    var description = document.getElementById('description').value;
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+    // Create new li element
+    var li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(description));
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Create del button element
+    var deleteBtn = document.createElement('button');
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-  // Append button to li
-  li.appendChild(deleteBtn);
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Append li to list
-  itemList.appendChild(li);
-}
+    // Append button to li
+    li.appendChild(deleteBtn);
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+    // Append li to list
+    itemList.appendChild(li);
     }
-  }
-}
 
-var itemsList = document.querySelectorAll(".list-group-item");
-itemsList.forEach(function(item) {
-  var editBtn = document.createElement("button");
-  editBtn.classList.add("btn", "btn-secondary", "btn-sm", "float-right", "edit", "mr-2");
-  editBtn.textContent = "Edit";
-  item.appendChild(editBtn);
-});
+    var itemsList = document.querySelectorAll(".list-group-item");
+    itemsList.forEach(function(item) {
+    var editBtn = document.createElement("button");
+    editBtn.classList.add("btn", "btn-secondary", "btn-sm", "float-right", "edit", "mr-2");
+    editBtn.textContent = "Edit";
+    item.appendChild(editBtn);
+    });
+
+
+    // Remove item
+    function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+        }
+    }
+    }
+
+   
+    function filterItems(e){    
+        // convert text to lowercase
+        var text = e.target.value.toLowerCase();
+        // Get lis
+        var items = itemList.getElementsByTagName('li');
+        // Convert to an array
+        Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        var description = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1|| description.toLocaleLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+        });
+    }
+    
