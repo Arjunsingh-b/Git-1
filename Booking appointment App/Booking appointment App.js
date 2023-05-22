8da75d6ab8a5a3e25e30547f25d0734053359d6c
Arjunsingh-b <arjunsingh.b1@gmail.com>
2023-05-22 21:30:49 +0530

    Lets store the users in the right format

--- Booking appointment App/Booking appointment App.js
+++ Booking appointment App/Booking appointment App.js
@@ -3,12 +3,14 @@ form.addEventListener('submit', submit);
 function submit(e){
     e.preventDefault();
     var name = document.getElementById('name').value;
-    
-    var email = document.getElementById('email').value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    console.log(localStorage.getItem('name'))
-    console.log(localStorage.getItem('email'))
 
-}
-var name1 = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+
+    let myObj ={
+        userName: name,
+        userEmail: email
+    };
+
+    let myObj_serialized = JSON.stringify(myObj);// now we have converted object to string
+    localStorage.setItem("myObj", myObj_serialized);// but it is all string and not an object so we need to parse it back to object
