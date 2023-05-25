f59bd313ee8ca9de2dfe50866ea47f78f28a1436
Arjunsingh-b <arjunsingh.b1@gmail.com>
2023-05-25 18:49:52 +0530

    Lets Scale the app to more users

--- Booking appointment App/Booking appointment App.js
+++ Booking appointment App/Booking appointment App.js
@@ -3,14 +3,26 @@ form.addEventListener('submit', submit);
 function submit(e){
     e.preventDefault();
     var name = document.getElementById('name').value;
-
+    //console.log(name1);
     var email = document.getElementById('email').value;
-
+    var combinedName = name+" "+email;
+    var itemList = document.getElementById('users');
+    //Create new li element
+    var li = document.createElement('li');
+    //Add class
+    li.className ='list-group-item';
+    li.appendChild(document.createTextNode(combinedName));
+    itemList.appendChild(li);
+  
 
     let myObj ={
         userName: name,
         userEmail: email
     };
 
-    let myObj_serialized = JSON.stringify(myObj);// now we have converted object to string
-    localStorage.setItem("myObj", myObj_serialized);// but it is all string and not an object so we need to parse it back to object
+    let formData = JSON.parse(localStorage.getItem('formDate')) || [];
+    formData.push({name, email});
+    localStorage.setItem('formDate', JSON.stringify(formData));
+    let a = JSON.parse(localStorage.getItem("formDate")) 
+    console.log(a);
+}
